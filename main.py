5c81d275aeb9e14aff7208884549af83a25275ef
bigV4 <neonewton91@gmail.com>
2021-12-15 21:19:45 +0800

    update

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ tab5 = tab.add(frm5, text="5.ppt工具")
 
 tab.pack(expand=True, fill=tk.BOTH)
 
-# 次顶层
-# 次顶层1,"https://smallpdf.com/cn/pdf-converter"
+# 二层
+# 二层1,"https://smallpdf.com/cn/pdf-converter"
 tab1_0 = ttk.Notebook(frm1)
 frm1_1 = tk.Frame(frm1, bg="red")
 tab1_1 = tab1_0.add(frm1_1, text="1.1转档&压缩&分割&合并")
@@ -54,8 +54,8 @@ frm1_4 = tk.Frame(frm1, bg="red")
 tab1_4 = tab1_0.add(frm1_4, text="1.4签署＆安全选项")
 tab1_0.pack(expand=True, fill=tk.BOTH)
 
-# 次次顶层
-# 次次顶层1.PDF工具1.1转档&压缩&分割&合并
+# 三层
+# 三层1.PDF工具1.1转档&压缩&分割&合并
 tab1_1_0 = ttk.Notebook(frm1_1)
 frm1_1_1 = tk.Frame(frm1_1, bg="red")
 tab1_1_1 = tab1_1_0.add(frm1_1_1, text="1.1.1压缩PDF")
@@ -65,7 +65,7 @@ frm1_1_3 = tk.Frame(frm1_1, bg="red")
 tab1_1_3 = tab1_1_0.add(frm1_1_3, text="1.1.3合并PDF")
 tab1_1_0.pack(expand=True, fill=tk.BOTH)
 
-# 次次顶层1.PDF工具1.2检视＆编辑
+# 三层1.PDF工具1.2检视＆编辑
 tab1_2_0 = ttk.Notebook(frm1_2)
 frm1_2_1 = tk.Frame(frm1_2, bg="red")
 tab1_2_1 = tab1_2_0.add(frm1_2_1, text="1.2.1PDF排页数")
@@ -75,7 +75,12 @@ frm1_2_3 = tk.Frame(frm1_2, bg="red")
 tab1_2_3 = tab1_2_0.add(frm1_2_3, text="1.2.3旋转PDF")
 tab1_2_0.pack(expand=True, fill=tk.BOTH)
 
-# 设置选中tab2
-tab.select(frm2)
+# 设置选中tab1
+tab.select(frm1)
 
+# 1.1.2分割PDF
+f1_1_2_b1 = tk.Button(frm1_1_2, text="开始切割按钮")
+f1_1_2_b1.pack()
+
+# 主界面循环
 root.mainloop()
